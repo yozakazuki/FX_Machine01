2eab590c06167d8bacee155830f6fe865072af1c
e125714 <e125714@172.31.2.138>
2014-05-13 22:17:07 +0900

    test2

--- src/applicatiion/God.java
+++ src/applicatiion/God.java
@@ -3,6 +3,7 @@ package applicatiion;
 public class God {
 	public static void main(String[] args) {
 		System.out.println("HEllO WORLD");
+		System.out.println("HEllO WORLD1");
 	}
 
 	public God() {
